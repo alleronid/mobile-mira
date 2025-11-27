128622946021f7cabedba8810bb63e8053346f23
anon <anon@naiv.dev>
2025-11-27 08:57:27 +0700

    Change End Point APi

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -3,70 +3,70 @@ class AppConstants {
   // static const String baseUrl = 'https://www.sen.point-cash.org';
   // static const String baseUrl = 'https://readypos.razinsoft.com';
   // static const String baseUrl = 'https://pos.digi-nest.my.id';
-  static const String baseUrl = 'https://my-mirra.com';
+  static const String baseUrl = 'https://app.my-mirra.com/api';
   //  static const String baseUrl = 'http://192.168.0.80:8080';
 
-  static const String loginUrl = '$baseUrl/api/sign-in';
-  static const String profileUpdate = '$baseUrl/api/profile/update';
-  static const String passwordChange = '$baseUrl/api/change/password';
-  static const String dashboard = '$baseUrl/api/dashboard';
-  static const String posProducts = '$baseUrl/api/product/search';
-  static const String customers = '$baseUrl/api/customer/search';
-  static const String cupon = '$baseUrl/api/apply/promo/code';
-  static const String customersGroup = '$baseUrl/api/customer/groups';
-  static const String addCustomer = '$baseUrl/api/customer/store';
-  static const String posStore = '$baseUrl/api/pos/store';
-  static const String report = '$baseUrl/api/reports';
+  static const String loginUrl = '$baseUrl/sign-in';
+  static const String profileUpdate = '$baseUrl/profile/update';
+  static const String passwordChange = '$baseUrl/change/password';
+  static const String dashboard = '$baseUrl/dashboard';
+  static const String posProducts = '$baseUrl/product/search';
+  static const String customers = '$baseUrl/customer/search';
+  static const String cupon = '$baseUrl/apply/promo/code';
+  static const String customersGroup = '$baseUrl/customer/groups';
+  static const String addCustomer = '$baseUrl/customer/store';
+  static const String posStore = '$baseUrl/pos/store';
+  static const String report = '$baseUrl/reports';
   // category
-  static const String categories = '$baseUrl/api/categories';
+  static const String categories = '$baseUrl/categories';
   static const String parentCategory =
-      '$baseUrl/api/categories/parent-category';
-  static const String addCategory = '$baseUrl/api/categories/store';
-  static const String updateCategory = '$baseUrl/api/categories/update';
-  static const String deleteCategory = '$baseUrl/api/categories/delete';
+      '$baseUrl/categories/parent-category';
+  static const String addCategory = '$baseUrl/categories/store';
+  static const String updateCategory = '$baseUrl/categories/update';
+  static const String deleteCategory = '$baseUrl/categories/delete';
   // product
-  static const String products = '$baseUrl/api/products';
-  static const String productDetails = '$baseUrl/api/product/details';
-  static const String deleteProduct = '$baseUrl/api/product/delete';
-  static const String productInfo = '$baseUrl/api/add/product/info';
-  static const String addProduct = '$baseUrl/api/product/store';
+  static const String products = '$baseUrl/products';
+  static const String productDetails = '$baseUrl/product/details';
+  static const String deleteProduct = '$baseUrl/product/delete';
+  static const String productInfo = '$baseUrl/add/product/info';
+  static const String addProduct = '$baseUrl/product/store';
   // brand
-  static const String brands = '$baseUrl/api/brands';
-  static const String addBrand = '$baseUrl/api/brands/store';
-  static const String updateBrand = '$baseUrl/api/brands/update';
-  static const String deleteBrand = '$baseUrl/api/brands/delete';
+  static const String brands = '$baseUrl/brands';
+  static const String addBrand = '$baseUrl/brands/store';
+  static const String updateBrand = '$baseUrl/brands/update';
+  static const String deleteBrand = '$baseUrl/brands/delete';
 
   // deposit
-  static const String bankAccount = '$baseUrl/api/accounts';
-  static const String balance = '$baseUrl/api/balance';
-  static const String balanceTransfer = '$baseUrl/api/balance/transfer';
+  static const String bankAccount = '$baseUrl/accounts';
+  static const String balance = '$baseUrl/balance';
+  static const String balanceTransfer = '$baseUrl/balance/transfer';
 
   // purchase
-  static const String purchasePDF = '$baseUrl/api/purchase/pdf';
-  static const String salesPDF = '$baseUrl/api/sale/pdf';
-  static const String statusPurchase = '$baseUrl/api/detail/{purchaseId}';
+  static const String purchasePDF = '$baseUrl/purchase/pdf';
+  static const String salesPDF = '$baseUrl/sale/pdf';
+  static const String statusPurchase = '$baseUrl/detail/{purchaseId}';
 
   // warehouse
-  static const String warehouses = '$baseUrl/api/warehouses';
-  static const String updateWarehouse = '$baseUrl/api/warehouses/update';
-  static const String addWarehouse = '$baseUrl/api/warehouses/store';
-  static const String deleteWarehouse = '$baseUrl/api/warehouses/delete';
+  static const String warehouses = '$baseUrl/warehouses';
+  static const String updateWarehouse = '$baseUrl/warehouses/update';
+  static const String addWarehouse = '$baseUrl/warehouses/store';
+  static const String deleteWarehouse = '$baseUrl/warehouses/delete';
 
   static const String democurrency = '\$';
-  static const String appcurrency = '$baseUrl/api/general-settings';
+  static const String appcurrency = '$baseUrl/general-settings';
 
-  static const String purchase = "$baseUrl/api/purchase";
-  static const String wareHouse = "$baseUrl/api/warehouses";
-  static const String sales = "$baseUrl/api/sales";
-  static const String expenses = "$baseUrl/api/expense";
-  static const String accounts = "$baseUrl/api/admin/accounts";
-  static const String drafts = "$baseUrl/api/drafts";
-  static const String paymentSuccess = "$baseUrl/api/sale/payment/success";
-  static const String deleteDraft = "$baseUrl/api/draft/delete";
+  static const String purchase = "$baseUrl/purchase";
+  static const String wareHouse = "$baseUrl/warehouses";
+  static const String sales = "$baseUrl/sales";
+  static const String expenses = "$baseUrl/expense";
+  static const String accounts = "$baseUrl/admin/accounts";
+  static const String drafts = "$baseUrl/drafts";
+  static const String paymentSuccess = "$baseUrl/sale/payment/success";
+  static const String deleteDraft = "$baseUrl/draft/delete";
   // payment
-  static const String paymentList = "$baseUrl/api/payment-gateway-list";
+  static const String paymentList = "$baseUrl/payment-gateway-list";
   // load invoice
-  static const String loadInvoice = "$baseUrl/api/invoice";
+  static const String loadInvoice = "$baseUrl/invoice";
 
   // Hive Box
   static const String appSettingsBox = 'appSettings';
